c5e56e5c84fe9a83fcd07558e326b35fd374b85c
SelimMeske <mesic.selim@outlook.com>
2020-07-03 15:12:42 +0200

    heroku updates 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
+let NGROK_URL = 'http://4922f0d1a9f2.ngrok.io/api/';
+let LOCALHOST_URL = 'http://localhost:3000/api/';
 export const environment = {
-  production: true
+  production: true,
+  SERVER_URL: NGROK_URL
 };
